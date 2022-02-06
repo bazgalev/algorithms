ac8ea370972e09418d8d74862965bfb372f9c373
getmikhail <mp@imagespark.ru>
2022-02-06 14:24:09 +0300

    Create phpunit config

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM php:8.1-cli
-COPY . /srv/app
-WORKDIR /srv/app
-CMD [ "php", "./src/test.php" ]
---
+++ tests/Unit/BubbleSortTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BubbleSortTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function sorted_correctly(): void
+    {
+        $this->assertTrue(true);
+    }
+}
